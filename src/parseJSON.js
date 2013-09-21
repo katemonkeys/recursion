75fc6b1d03cd9d25ad7ef1f4f7d3267fc2a6a6f8
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-21 16:08:17 -0700

    the json string being passed to parse doesn't have a length method so I can't begin to manipulate it. Some work with regexpressions.
    Beginning "getElementsByClassName" and we'll see.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,24 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
+// Same ridiculous array as stringify:
+//"{"a":[4,5,2,"panda"],"q":[1,2,3],"cats":[[1,2,3],[4,5,2,"panda"]],"germany":{"great":"scott"}}"
+
+//Ditch the outer "" and check if/that the first and last remaining characters are {}.
+//Look for the first string marking the first property: the regexp will be /"\w+":/
+//Look for the string matching the first value: /:[\w,\d\[\]\{\}"]+/ taking off the first : and the last character only if it is a comma
+//(using substrings) 
+//"a":[4,5,2,"panda"], "q":[1,2,3], "cats":[[1,2,3],[4,5,2,"panda"]], "germany":{"great":"scott"}}
+
 var parseJSON = function (json) {
-  // your code goes here
+
+// I had intended for the following to chop off the first and last double quotes but length isn't a method even on strings,
+//which JSON objects inherently are!!
+//	var newString = json.toString();
+//	var content = newString.substring(1, newString.length()-1);
+//	if (json.charAt(0) === "{" && json.charAt(json.length-1) === "}") {
+		//then it's an object and we can start going through it that way
+//	}
+  	return json;
 };
