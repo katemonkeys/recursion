a59847e4c2dc73037ea461ddd4505c4e79e831d3
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-21 18:32:27 -0700

    I don't understand why this doesn't work. It even uses recursion! Why does the debugger say that seek is undefined? It's the same syntax I was using yesterday in twittler.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,47 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-  // your code here
+// But instead we're going to implement it from scratch:
+var getElementsByClassName = function (classname) {
+  var where = document.documentElement;
+  var nodeList = [];
+
+  var searchChildren = function(location) {
+  	debugger;
+  	var seek = location.children;
+  	console.log(seek);
+  	for (var i=0; i<seek.length; i++) {
+  		if (seek[i].className === classname) {
+  			nodeList.push(seek[i]);
+  			console.log(seek[i]);
+  		}
+  	}
+  	if (seek.length > 1) {
+  		searchChildren(seek);
+  	}
+  };
+  searchChildren(where);
+  return nodeList;
 };
+
+//Internet:
+// document.getElementsByClassName = function(cl) {
+// var retnode = [];
+// var myclass = new RegExp('\\b'+cl+'\\b');
+// var elem = this.getElementsByTagName('*');
+// for (var i = 0; i < elem.length; i++) {
+// var classes = elem[i].className;
+// if (myclass.test(classes)) retnode.push(elem[i]);
+// }
+// return retnode;
+// }; 
+
+// var htmlStrings = [
+//   "<p class='targetClassName'></p>",
+//   "<p class='otherClassName targetClassName'></p>",
+//   "<p><p class='targetClassName'></p></p>",
+//   "<p><p class='targetClassName'><p class='targetClassName'></p></p></p>",
+//   "<p><p></p><p><p class='targetClassName'></p></p></p>",
+//   "<p><p class='targetClassName'></p><p class='targetClassName'></p></p>",
+//   "<p><div class='somediv'><div class='innerdiv'><span class='targetClassName'>yay</span></div></div></p>"
+// ];
