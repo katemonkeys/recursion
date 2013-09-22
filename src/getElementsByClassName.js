9f8f9839ca01fecc1ae60c1e836631146f4ee3a5
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-09-21 20:04:04 -0700

    getElementsByClass doesn't work but it's closer. Still going to try parseJSON.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,44 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function (classname) {
   var where = document.documentElement;
-  var nodeList = [];
-
-  var searchChildren = function(location) {
-  	debugger;
-  	var seek = location.children;
-  	console.log(seek);
+  
+  var searchChildren = function(location,classname) {
+  	var nodeList = [];
+	var seek = location.children;
+  
   	for (var i=0; i<seek.length; i++) {
-  		if (seek[i].className === classname) {
+  		if (seek[i].className.match(classname) !== null) {
   			nodeList.push(seek[i]);
   			console.log(seek[i]);
   		}
   	}
-  	if (seek.length > 1) {
-  		searchChildren(seek);
+  	if (seek.nodeType === 1 && seek.length > 0) {
+  		searchChildren(seek,classname);
   	}
+	return nodeList;
   };
-  searchChildren(where);
-  return nodeList;
+   return searchChildren(where);
+
 };
-
-//Internet:
-// document.getElementsByClassName = function(cl) {
-// var retnode = [];
-// var myclass = new RegExp('\\b'+cl+'\\b');
-// var elem = this.getElementsByTagName('*');
-// for (var i = 0; i < elem.length; i++) {
-// var classes = elem[i].className;
-// if (myclass.test(classes)) retnode.push(elem[i]);
-// }
-// return retnode;
-// }; 
-
-// var htmlStrings = [
-//   "<p class='targetClassName'></p>",
-//   "<p class='otherClassName targetClassName'></p>",
-//   "<p><p class='targetClassName'></p></p>",
-//   "<p><p class='targetClassName'><p class='targetClassName'></p></p></p>",
-//   "<p><p></p><p><p class='targetClassName'></p></p></p>",
-//   "<p><p class='targetClassName'></p><p class='targetClassName'></p></p>",
-//   "<p><div class='somediv'><div class='innerdiv'><span class='targetClassName'>yay</span></div></div></p>"
-// ];
